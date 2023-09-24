2d4d078976c7913c92a6be873b4adfda344dd7e3
Lena Yoo <lenayoo17@gmail.com>
2023-09-25 06:37:17 +0900

    add fortune cookie image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import cookie from './images/main-cookie.png'
 
 function App() {
   return (
     <div className="App">
       <h1>hello my day</h1>
+      <img src={cookie} alt="main" />
     </div>
   )
 }
