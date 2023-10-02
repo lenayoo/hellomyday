a849697bae063932cd40f7656affa951d4464a86
Lena Yoo <lenayoo17@gmail.com>
2023-10-02 22:39:03 +0900

    add cookies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css'
 import cookie from './images/main-cookie.png'
+import fortunes from './fortunecookie'
+import { useState } from 'react'
 
 function App() {
+  const [todayFortune, setTodayFortune] = useState('')
+
+  const randomFortune = () => {
+    let randomIndex = Math.floor(Math.random() * fortunes.length)
+    console.log(randomIndex)
+    console.log(fortunes[randomIndex])
+    return setTodayFortune(fortunes[randomIndex])
+  }
+
   return (
     <div className="App">
       <h1 className="header-text">hello my day</h1>
       <img src={cookie} alt="main" className="cookie" />
       <div>
-        <button className="btn-cookie">Open your luck</button>
+        <button className="btn-cookie" onClick={randomFortune}>
+          Open your luck
+        </button>
       </div>
+      <div className="text-cookie">{todayFortune}</div>
     </div>
   )
 }
